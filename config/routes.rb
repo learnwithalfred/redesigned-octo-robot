02d79f991ec965fc69d40d23af6eaf267515321a
Alfred Boateng <programmingwithalfred@gmail.com>
2022-08-28 03:42:46 +0000

    Add home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get "homepage/index"
   resources :announcements
   resources :subjects
   resources :homes
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "homepage#index"
 end
