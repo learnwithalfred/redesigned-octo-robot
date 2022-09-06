946133c7772b0a9c5502dfecd433ccabb3231a6c
Alfred Boateng <programmingwithalfred@gmail.com>
2022-09-06 01:18:44 +0000

    Update subject controller

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -4,18 +4,15 @@ class SubjectsController < ApplicationController
   before_action :set_subject, only: %i[ show edit update destroy ]
   # before_action :is_school_admin, except: %i[ index show]
 
-  # GET /subjects or /subjects.json
   def index
     subjects = Subject.all.order("name asc")
     render status: :ok, json: subjects.to_json
   end
 
-  # GET /subjects/1 or /subjects/1.json
   def show
     render status: :ok, json: @subject.to_json
   end
 
-  # POST /subjects or /subjects.json
   def create
     binding.break
     subject = Subject.new(subject_params)
@@ -24,31 +21,22 @@ class SubjectsController < ApplicationController
     respond_with_success(t("successfully_created", entity: "Subject"))
   end
 
-  # PATCH/PUT /subjects/1 or /subjects/1.json
   def update
-    binding.break
     @subject.update!(subject_params)
-    respond_with_success(t("successfully_updated"))
+    respond_with_success(t("successfully_updated", entity: "Subject"))
   end
 
-  # DELETE /subjects/1 or /subjects/1.json
   def destroy
-    @subject.destroy
-
-    respond_to do |format|
-      format.html { redirect_to subjects_url, notice: "Subject was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    @subject.destroy!
+    respond_with_json
   end
 
   private
 
-    # Use callbacks to share common setup or constraints between actions.
     def set_subject
       @subject = Subject.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def subject_params
       params.require(:subject).permit(:name)
     end
