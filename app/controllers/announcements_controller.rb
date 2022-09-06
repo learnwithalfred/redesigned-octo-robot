1416f78ae5330d6a2628f0945d7167549a9834e9
Alfred Boateng <programmingwithalfred@gmail.com>
2022-09-06 01:41:50 +0000

    Update announcement controllers

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -4,49 +4,31 @@ class AnnouncementsController < ApplicationController
   before_action :set_announcement, only: %i[ show update destroy ]
   # before_action :is_school_admin, except: %i[ index show]
 
-  # GET /announcements or /announcements.json
   def index
     announcements = Announcement.all.order("created_at DESC")
     render status: :ok, json: { announcements: announcements }
   end
 
-  # GET /announcements/1 or /announcements/1.json
   def show
     announcement = Announcement.find(params[:id])
     render status: :ok, json: { announcement: announcement }
   end
 
-  # POST /announcements or /announcements.json
   def create
     @announcement = Announcement.new(announcement_params)
+    @announcement.save!
 
-    respond_to do |format|
-      if @announcement.save
-        format.html { redirect_to announcement_url(@announcement), notice: "Announcement was successfully created." }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-      end
-    end
+    respond_with_success(t("successfully_created", entity: "Announcement"))
   end
 
-  # PATCH/PUT /announcements/1 or /announcements/1.json
   def update
-    respond_to do |format|
-      if @announcement.update(announcement_params)
-        format.html { redirect_to announcement_url(@announcement), notice: "Announcement was successfully updated." }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-      end
-    end
+    @announcement.update!(announcement_params)
+    respond_with_success(t("successfully_updated", entity: "Announcement"))
   end
 
-  # DELETE /announcements/1 or /announcements/1.json
   def destroy
     @announcement.destroy
-
-    respond_to do |format|
-      format.html { redirect_to announcements_url, notice: "Announcement was successfully destroyed." }
-    end
+    respond_with_json
   end
 
   private
